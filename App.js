6f896f7d4975b559a1f1f69cc2b44827bf7104af
Michael Liendo <mtliendo@gmail.com>
2020-09-14 11:52:27 -0500

    challenge complete but so so messy!

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { gql, useQuery } from "@apollo/client";
+import { ListItem, Input } from "react-native-elements";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { Formik } from "formik";
 
 const GITHUB_TOKEN = "49fe420ef16ba4140865b60b1c2ba46227afa46b";
 const client = new ApolloClient({
@@ -18,10 +21,13 @@ const client = new ApolloClient({
 const GET_REPOS = gql`
   query {
     viewer {
-      repositories(last: 10) {
-        cursor
+      repositories(last: 30) {
         edges {
+          cursor
           node {
+            owner {
+              login
+            }
             id
             name
           }
@@ -45,7 +51,7 @@ const LoginScreen = ({ navigation }) => {
   );
 };
 
-const HomeScreen = ({ route }) => {
+const HomeScreen = ({ route, navigation }) => {
   const { data, loading, error } = useQuery(GET_REPOS);
 
   if (loading) return <Text>Loading..</Text>;
@@ -54,8 +60,72 @@ const HomeScreen = ({ route }) => {
     return <Text>ERROR</Text>;
   }
   if (!data) return <Text>Not found</Text>;
-  console.log(data);
-  return <Text>I am the home screen {route.params.githubToken}</Text>;
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("RepoFormScreen", {
+            repoName: item.node.name,
+            accessToken: GITHUB_TOKEN,
+            owner: item.node.owner.login,
+          });
+        }}
+      >
+        <ListItem key={item} bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{item.node.name}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <FlatList
+      data={data.viewer.repositories.edges}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.node.id}
+    />
+  );
+};
+
+export const RepoFormScreen = ({ route }) => {
+  const handleFormSubmit = (values) => {
+    console.log("the values", values);
+    fetch(
+      `https://api.github.com/repos/${route.params.owner}/${route.params.repoName}/issues`,
+      {
+        method: "post",
+        headers: { Authorization: `Bearer ${route.params.accessToken}` },
+        body: JSON.stringify({ title: values.title, body: values.body }),
+      }
+    )
+      .then((res) => res.json())
+      .then((status) => console.log("the status", status))
+      .catch((e) => console.log("the error", e));
+  };
+
+  return (
+    <Formik initialValues={{ title: "", body: "" }} onSubmit={handleFormSubmit}>
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <Input
+            placeholder="Issue Title"
+            onChangeText={handleChange("title")}
+            onBlur={handleBlur("title")}
+            value={values.title}
+          />
+          <Input
+            placeholder="Issue Description"
+            onChangeText={handleChange("body")}
+            onBlur={handleBlur("body")}
+            value={values.body}
+          />
+          <Button onPress={handleSubmit} title="Create Issue" />
+        </View>
+      )}
+    </Formik>
+  );
 };
 
 const Stack = createStackNavigator();
@@ -67,6 +137,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="RepoFormScreen" component={RepoFormScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </ApolloProvider>
